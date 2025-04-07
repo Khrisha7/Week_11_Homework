29ec6bd09c43c43d5a547a0fd40551939a4e241b
Aiman Ahmed <aimanahmed61@gmail.com>
2025-04-07 15:53:59 +0100

    EDIT: modified routes.py

--- application/routes.py
+++ application/routes.py
@@ -31,7 +31,6 @@ def login():
     return render_template('login.html')  # Render the login form on GET request
 
 def send_contact_message(name, email, message):
-    # For example, print the message to the console (you could also send an email or save it to a database)
     print(f"Name: {name}\nEmail: {email}\nMessage: {message}")
     # Additional logic for sending the message or storing it
 
